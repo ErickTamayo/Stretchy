66f7a843aeb3b82da6b371878a6307ec7f94f0b2
Erick Tamayo <ericktamayo@esd.com.do>
2015-01-26 20:45:06 -0400

    added Document Builder tests

--- tests/Document/DocumentBuilderTest.php
+++ tests/Document/DocumentBuilderTest.php
@@ -26,6 +26,60 @@ class DocumentBuilderTest extends PHPUnit_Framework_TestCase {
 		$builder->index('foo')->type('bar')->insert(['tamayo' => 'stretchy']);
 	}
 
+	public function testBuildUpdate()
+	{
+		$connection = Mockery::mock('Tamayo\Stretchy\Connection');
+		$connection->shouldReceive('getIndexPrefix')->once();
+		$connection->shouldReceive('documentUpdate')->once();
+
+		$grammar = Mockery::mock('Tamayo\Stretchy\Document\Grammar');
+		$grammar->shouldReceive('setIndexPrefix')->once();
+		$grammar->shouldReceive('compileUpdate')->once();
+
+		$processor = Mockery::mock('Tamayo\Stretchy\Document\Processor');
+		$processor->shouldReceive('processUpdate')->once();
+
+		$builder = new Builder($connection, $grammar, $processor);
+
+		$builder->index('foo')->type('bar')->update(['tamayo' => 'stretchy']);
+	}
+
+	public function testBuildDelete()
+	{
+		$connection = Mockery::mock('Tamayo\Stretchy\Connection');
+		$connection->shouldReceive('getIndexPrefix')->once();
+		$connection->shouldReceive('documentDelete')->once();
+
+		$grammar = Mockery::mock('Tamayo\Stretchy\Document\Grammar');
+		$grammar->shouldReceive('setIndexPrefix')->once();
+		$grammar->shouldReceive('compileDelete')->once();
+
+		$processor = Mockery::mock('Tamayo\Stretchy\Document\Processor');
+		$processor->shouldReceive('processDelete')->once();
+
+		$builder = new Builder($connection, $grammar, $processor);
+
+		$builder->index('foo')->type('bar')->delete();
+	}
+
+	public function testBuildGet()
+	{
+		$connection = Mockery::mock('Tamayo\Stretchy\Connection');
+		$connection->shouldReceive('getIndexPrefix')->once();
+		$connection->shouldReceive('documentGet')->once();
+
+		$grammar = Mockery::mock('Tamayo\Stretchy\Document\Grammar');
+		$grammar->shouldReceive('setIndexPrefix')->once();
+		$grammar->shouldReceive('compileGet')->once();
+
+		$processor = Mockery::mock('Tamayo\Stretchy\Document\Processor');
+		$processor->shouldReceive('processGet')->once();
+
+		$builder = new Builder($connection, $grammar, $processor);
+
+		$builder->index('foo')->type('bar')->get();
+	}
+
     /**
      * @expectedException Tamayo\Stretchy\Exceptions\IndexMustBeDefinedException
      */
